86cda2740c2a1ba8e42d6021e226fb2c5f6d82c8
AUGIE-PC <fifoaugie06@gmail.com>
2020-09-10 23:06:15 +0700

    Move routing augie + Fitur update perangkat desa, bpd

--- app/Http/Controllers/Admin/BPDController.php
+++ app/Http/Controllers/Admin/BPDController.php
@@ -40,6 +40,41 @@ class BPDController extends Controller
         return redirect()->route('admin_home_bpd')->with('status', 'Data Berhasil Ditambahkan');
     }
 
+    public function update(Request $request, $id)
+    {
+        $file = $request->file('photo');
+
+        if (is_null($file)) {
+            BPD::where('id', $id)
+                ->update([
+                    'nama' => $request->nama,
+                    'jabatan' => $request->jabatan
+                ]);
+        }else{
+            $validator = Validator::make($request->all(), [
+                'photo' => 'required|file|image|mimes:jpeg,png,jpg|max:2048'
+            ]);
+
+            if ($validator->fails()) {
+                return redirect()->route('admin_home_bpd')->with('alert', $validator->getMessageBag());
+            }
+
+            $file = $request->file('photo');
+            $nama_file = "BPD" . time() . "." . $file->getClientOriginalExtension();
+            $tujuan_upload = storage_path('app/public/images/bpd');
+            $file->move($tujuan_upload, $nama_file);
+
+            BPD::where('id', $id)
+                ->update([
+                    'nama' => $request->nama,
+                    'jabatan' => $request->jabatan,
+                    'photo' => $nama_file
+                ]);
+        }
+
+        return redirect()->route('admin_home_bpd')->with('status', 'Data Berhasil Diupdate');
+    }
+
     public function destroy($id)
     {
         BPD::destroy($id);
