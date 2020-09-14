8f040f4ace052395202702f36293759128a4202d
fifo augie <fifoaugie06@gmail.com>
2020-09-14 21:11:23 +0700

    Change structure folder

--- resources/views/Admin/Pages/Home/home_perangkat_desa.blade.php
+++ resources/views/Admin/Pages/Home/home_perangkat_desa.blade.php
@@ -1,19 +1,19 @@
 @extends('Admin.Template.all')
 
-@section('page_title','Badan Permusyawaratan Desa')
+@section('page_title','Perangkat Desa')
 
 @section('breadcumb')
     <li class="breadcrumb-item">Home</li>
-    <li class="breadcrumb-item">BPD</li>
+    <li class="breadcrumb-item">Perangkat Desa</li>
 @endsection
 
 @section('content')
     <div class="card">
         <div class="card-header">
             <div class="d-flex justify-content-between">
-                <h3 class="card-title m-2">Informasi keanggotaan BPD</h3>
-                <button class="btn btn-secondary" data-toggle="modal"
-                        data-target="#tambahPerangkatModal">Tambahkan Anggota
+                <h3 class="card-title m-2">Informasi mengenai Perangkat Desa</h3>
+                <button class="btn btn-primary" data-toggle="modal"
+                        data-target="#tambahPerangkatModal"><i class="fas fa-plus"></i> Tambah Perangkat
                 </button>
             </div>
 
@@ -42,24 +42,24 @@
                 </tr>
                 </thead>
                 <tbody>
-                @foreach($bpd as $bp)
+                @foreach($perangkats as $perangkat)
                     <tr>
                         <td class="text-center">{{$loop->iteration . "."}}</td>
                         <td class="text-center">
-                            <img src="{{url('storage/images/bpd') . "/" .$bp->photo}}"
-                                 class="rounded" alt="{{url('storage/images/bpd') . "/" .$bp->nama}}"
+                            <img src="{{url('storage/images/perangkat') . "/" .$perangkat->photo}}"
+                                 class="rounded" alt="{{url('storage/images/home') . "/" .$perangkat->nama}}"
                                  width="100" height="100">
                         </td>
-                        <td>{{$bp->nama}}</td>
-                        <td>{{$bp->jabatan}}</td>
+                        <td>{{$perangkat->nama}}</td>
+                        <td>{{$perangkat->jabatan}}</td>
                         <td class="text-center">
                             <button type="button" class="btn btn-warning btn-sm" data-toggle="modal"
-                                    data-target="#updateBpdModal" data-id="{{$bp->id}}"
-                                    data-nama="{{$bp->nama}}" data-jabatan="{{$bp->jabatan}}"
+                                    data-target="#updatePerangkatModal" data-id="{{$perangkat->id}}"
+                                    data-nama="{{$perangkat->nama}}" data-jabatan="{{$perangkat->jabatan}}"
                                     style="font-size: 12px; width: 60px; padding: 2px">Edit
                             </button>
-                            <button type="button" class="btn btn-danger btn-sm deleteBPDModal"
-                                    id="{{$bp->id}}" data-toggle="modal" data-target="#deleteBPDModal"
+                            <button type="button" class="btn btn-danger btn-sm deletePerangkatModal"
+                                    id="{{$perangkat->id}}" data-toggle="modal" data-target="#deletePerangkatModal"
                                     style="font-size: 12px; width: 60px; padding: 2px">Delete
                             </button>
                         </td>
@@ -73,19 +73,18 @@
         </div>
     </div>
 
-
     <!-- Add Perangkat Modal-->
     <div class="modal fade" id="tambahPerangkatModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
          aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Tambah Anggota BPD</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Tambah Perangkat Desa</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
-                <form action="{{route('admin_home_bpd_post')}}" method="post" enctype="multipart/form-data">
+                <form action="{{route('admin_home_perangkatdesa_post')}}" method="post" enctype="multipart/form-data">
                     @csrf
                     <div class="modal-body">
                         <input type="text" class="form-control mt-3 mb-3" aria-label="Sizing example input"
@@ -106,39 +105,41 @@
     </div>
 
     <!-- Update Perangkat Modal-->
-    <div class="modal fade" id="updateBpdModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    <div class="modal fade" id="updatePerangkatModal" tabindex="-1" aria-labelledby="exampleModalLabel"
          aria-hidden="true">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Tambah Anggota BPD</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
+                    <h5 class="modal-title" id="exampleModalLabel">Update Perangkat Desa</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <form id="formUpdateBpd" action="#" method="post" enctype="multipart/form-data">
-                    @method('PUT')
-                    @csrf
-                    <div class="modal-body">
-                        <input type="text" class="form-control mt-3 mb-3" aria-label="Sizing example input"
-                               aria-describedby="inputGroup-sizing-lg" id="nama"
-                               placeholder="Masukkan Nama" name="nama" required>
-                        <input type="text" class="form-control mt-3 mb-3" aria-label="Sizing example input"
-                               aria-describedby="inputGroup-sizing-lg"
-                               placeholder="Masukkan Jabatan" name="jabatan" id="jabatan" required>
-                        <input type="file" name="photo" class="mb-3">
-                    </div>
-                    <div class="modal-footer">
-                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                        <button class="btn btn-primary" type="submit">Update</button>
-                    </div>
-                </form>
+                <div class="modal-body">
+                    <form id="formUpdatePerangkat" action="#" method="post" enctype="multipart/form-data">
+                        @method('PUT')
+                        @csrf
+                        <div class="modal-body">
+                            <input type="text" class="form-control mt-3 mb-3" aria-label="Sizing example input"
+                                   aria-describedby="inputGroup-sizing-lg" id="nama"
+                                   placeholder="Masukkan Nama" name="nama" required>
+                            <input type="text" class="form-control mt-3 mb-3" aria-label="Sizing example input"
+                                   aria-describedby="inputGroup-sizing-lg"
+                                   placeholder="Masukkan Jabatan" name="jabatan" id="jabatan" required>
+                            <input type="file" name="photo" class="mb-3">
+                        </div>
+                        <div class="modal-footer">
+                            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                            <button class="btn btn-primary" type="submit">Update</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
+    </div>ss
 
     <!--Modal delete data-->
-    <div class="modal fade" id="deleteBPDModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+    <div class="modal fade" id="deletePerangkatModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
          aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -148,7 +149,7 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <form id="formDeleteBPD" action="#" method="post">
+                <form id="formDeletePerangkat" action="#" method="post">
                     @method('delete')
                     @csrf
                     <div class="modal-body">
@@ -177,13 +178,13 @@
                 "autoWidth": false
             });
 
-            $(".deleteBPDModal").click(function (e) {
+            $(".deletePerangkatModal").click(function (e) {
                 let id = $(this).attr("id")
 
-                $('#formDeleteBPD').attr('action', '/4dm1n/home/bpd/delete/' + id)
+                $('#formDeletePerangkat').attr('action', '/4dm1n/home/perangkatdesa/delete/' + id)
             });
 
-            $('#updateBpdModal').on('show.bs.modal', function (event) {
+            $('#updatePerangkatModal').on('show.bs.modal', function (event) {
                 let button = $(event.relatedTarget);
                 let id = button.data('id');
                 let nama = button.data('nama');
@@ -193,7 +194,7 @@
                 modal.find('#nama').val(nama)
                 modal.find('#jabatan').val(jabatan)
 
-                $('#formUpdateBpd').attr('action', '/4dm1n/home/bpd/update/' + id)
+                $('#formUpdatePerangkat').attr('action', '/4dm1n/home/perangkatdesa/update/' + id)
             });
 
             window.setTimeout(function () {
