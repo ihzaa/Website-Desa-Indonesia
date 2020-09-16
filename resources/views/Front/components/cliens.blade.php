3b919917952d01e18d7d7904148a9881eab3a32e
fifo augie <fifoaugie06@gmail.com>
2020-09-16 07:04:40 +0700

    صرخت وأنا أتخيل ، كم أنت قاسٍ عليّ

--- resources/views/Front/components/cliens.blade.php
+++ resources/views/Front/components/cliens.blade.php
@@ -3,28 +3,20 @@
 
         <div class="row" data-aos="zoom-in">
 
-            <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                <img src="{{asset('Front/img/clients/client-1.png')}}" class="img-fluid" alt="">
-            </div>
+            <div class="d-flex align-items-center justify-content-center">
 
-            <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                <img src="{{asset('Front/img/clients/client-2.png')}}" class="img-fluid" alt="">
-            </div>
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                    <img src="{{asset('Logo/logo_jatim.png')}}" class="img-fluid" alt="">
+                </div>
 
-            <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                <img src="{{asset('Front/img/clients/client-3.png')}}" class="img-fluid" alt="">
-            </div>
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                    <img src="{{asset('Logo/logo_garuda.png')}}" class="img-fluid" alt="">
+                </div>
 
-            <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                <img src="{{asset('Front/img/clients/client-4.png')}}" class="img-fluid" alt="">
-            </div>
+                <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                    <img src="{{asset('Logo/logo_tulungagung.png')}}" class="img-fluid" alt="">
+                </div>
 
-            <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                <img src="{{asset('Front/img/clients/client-5.png')}}" class="img-fluid" alt="">
-            </div>
-
-            <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-                <img src="{{asset('Front/img/clients/client-6.png')}}" class="img-fluid" alt="">
             </div>
 
         </div>
