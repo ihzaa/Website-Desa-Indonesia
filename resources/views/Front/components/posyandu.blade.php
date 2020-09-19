bb8ef58ea73f2a31317f9caf4a9111a697c4b247
Byas Ageng Haqiqie <byashaqiqie48@gmail.com>
2020-09-19 14:13:36 +0700

    add posyandu blade

--- resources/views/Front/components/posyandu.blade.php
+++ resources/views/Front/components/posyandu.blade.php
@@ -3,9 +3,7 @@
 
         <div class="section-title">
             <h2>Posyandu Desa</h2>
-            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
-                consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
-                fugiat sit in iste officiis commodi quidem hic quas.</p>
+            <p>Belum Ada Data</p>
         </div>
 
         <div class="row">
